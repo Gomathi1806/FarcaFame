c7c62d859083b06c9418b1d85acaade096bc7af6
Gomathi T <62291940+Gomathi1806@users.noreply.github.com>
2024-03-24 15:47:32 +0000

    Create player.js

---
+++ src/components/player.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Player } from '@livepeer/react'
+
+const VideoPlayer = () => {
+  return <Player src='YOUR_STREAM_ID' autoplay controls />
+}
+
+export default VideoPlayer
